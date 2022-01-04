fa15cfca62f420a9e9786f0c0ff42e171e005bc0
Nhung Nguyen <nhungdoca3k41@gmail.com>
2022-01-04 12:00:49 +0700

    fix

--- api/index.js
+++ api/index.js
@@ -8,20 +8,22 @@ const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
-var cors = require('cors')
+var cors = require("cors");
 
 dotenv.config();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose
-    .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-
-    })
+    .connect(
+        "mongodb+srv://quang:1234@cluster0.v2ueh.mongodb.net/blogapp?retryWrites=true&w=majority",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
     .then(console.log("Connected to MongoDB"))
     .catch((err) => console.log(err));
 
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -7,23 +7,23 @@ import axios from "axios";
 import { useLocation } from "react-router";
 
 export default function Home() {
-  const [posts, setPosts] = useState([]);
-  const { search } = useLocation();
+    const [posts, setPosts] = useState([]);
+    const { search } = useLocation();
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const res = await axios.get("/posts" + search);
-      setPosts(res.data);
-    };
-    fetchPosts();
-  }, [search]);
-  return (
-    <>
-      <Header />
-      <div className="home">
-        <Posts posts={posts} />
-        <Sidebar />
-      </div>
-    </>
-  );
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const res = await axios.get("http://localhost:5000/api/posts" + search);
+            setPosts(res.data);
+        };
+        fetchPosts();
+    }, [search]);
+    return (
+        <>
+            <Header />
+            <div className="home">
+                <Posts posts={posts} />
+                <Sidebar />
+            </div>
+        </>
+    );
 }
